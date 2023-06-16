aeafb45230a72d9d2d97945195bb833120069773
pabloromjim <106261473+pabloromjim@users.noreply.github.com>
2023-06-16 14:01:10 +0200

    Update 2017_05_06_231543_create_turns_table.php

--- database/migrations/2017_05_06_231543_create_turns_table.php
+++ database/migrations/2017_05_06_231543_create_turns_table.php
@@ -16,7 +16,7 @@ class CreateTurnsTable extends Migration
         Schema::create('turns', function (Blueprint $table) {
             $table->integer('id');
             $table->integer('player_id')->unsigned();
-            $table->foreign('player_id')->references('id')->on('users');
+            //$table->foreign('player_id')->references('id')->on('users');
             $table->integer('game_id')->unsigned();
             $table->foreign('game_id')->references('id')->on('games');
             $table->enum('location',[1,2,3,4,5,6,7,8,9])->nullable();
